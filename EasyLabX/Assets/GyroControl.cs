d5ad1808c5d71f3b9b2eefff035e6fd57ba77d91
n0sc0p3 <58154057+vvvinit@users.noreply.github.com>
2022-03-30 19:15:52 +0530

    Added start-menu scene and onClick scripts
    Added 2 empty scenes for 2 chemistry experiments

---
+++ EasyLabX/Assets/GyroControl.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class GyroControl : MonoBehaviour
+{
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+
+    private GameObject cameraContainer;
+    private Quaternion rotation;
+
+    /*
+     * Start fn is called before the first frame update
+     */
+    void Start()
+    {
+        cameraContainer = new GameObject("Camera Container");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+
+        gyroEnabled = EnableGyro();
+    }
+
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+
+            cameraContainer.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
+            rotation = new Quaternion(0, 0, 1, 0);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /* 
+     * Update is called once per frame
+     */
+    void Update()
+    {
+        if (gyroEnabled)
+        {
+            transform.localRotation = gyro.attitude * rotation;
+        }
+    }
+}
