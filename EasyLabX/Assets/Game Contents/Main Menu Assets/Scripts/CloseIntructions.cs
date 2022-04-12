e7b0999ac46647941012e90e437e8a4ed50962c7
n0sc0p3 <58154057+vvvinit@users.noreply.github.com>
2022-04-12 12:55:38 +0530

    Added Laboratory Template

---
+++ EasyLabX/Assets/Game Contents/Main Menu Assets/Scripts/CloseIntructions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseIntructions : MonoBehaviour {
+
+	public GameObject mainMenu;
+	public GameObject instructions;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+		mainMenu.SetActive (false);
+		instructions.SetActive (true);
+	}
+	
+
+	public void OpenMenu ()
+    {
+		mainMenu.SetActive (true);
+		Destroy (instructions.gameObject);
+	}
+}
