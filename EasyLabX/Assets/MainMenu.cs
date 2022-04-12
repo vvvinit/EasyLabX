0cdacea02aef9a68ff8888e117948d93f193e86b
n0sc0p3 <58154057+vvvinit@users.noreply.github.com>
2022-04-12 18:20:56 +0530

    Added Aim View in Experiments menu

--- EasyLabX/Assets/MainMenu.cs
+++ EasyLabX/Assets/MainMenu.cs
@@ -10,9 +10,48 @@ public class MainMenu : MonoBehaviour
      * Make sure you add the E1 scene in the build settings.
      * The build index offset should be set accordingly.
      */
+
+    int delayTime = 25;
+
+    
+
+    IEnumerator SetDelay(int time)
+    {
+        yield return new WaitForSeconds(time);
+    }
+
+
     public void LoadScene(string s)
     {
         SceneManager.LoadScene(s);
+    }
+
+    public void StartExperimentL1(GameObject g)
+    {
+        g.SetActive(true);
+        StartCoroutine(SetDelay(delayTime));
+        LoadScene("L1");
+    }
+
+    public void StartExperimentL2(GameObject g)
+    {
+        g.SetActive(true);
+        StartCoroutine(SetDelay(delayTime));
+        LoadScene("L2");
+    }
+
+    public void StartExperimentL3(GameObject g)
+    {
+        g.SetActive(true);
+        StartCoroutine(SetDelay(delayTime));
+        LoadScene("L3");
+    }
+
+    public void StartExperimentL4(GameObject g)
+    {
+        g.SetActive(true);
+        StartCoroutine(SetDelay(delayTime));
+        LoadScene("L4");
     }
 
     public void QuitApplication()
